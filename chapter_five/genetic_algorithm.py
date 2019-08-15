1e50119501cb8cd2dc3fd9a0443c719605ede301
badarithm <karolis.markevicius@protonmail.ch>
2019-08-15 11:06:14 +0200

    ch5: genetic algorithm

---
+++ chapter_five/genetic_algorithm.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+from typing import TypeVar, Generic, List, Tuple, Callable
+from enum import Enum
+from random import choices, random
+from heapq import nlargest
+from statistics import mean
+from chapter_five.chromosome import Chromosome
+
+C = TypeVar('C', bound='Chromosome')
+
+
+class GeneticAlgorithm(Generic[C]):
+    SelectionType = Enum("SelectionType", "ROULETTE TOURNAMENT")
+
+    def __init__(self, initial_population: List[C], threshold: float, max_generations: int = 100,
+                 mutation_chance: float = 0.1, crossover_chance: float = 0.7, selection_type: SelectionType = SelectionType.TOURNAMENT) -> None:
+        self._population: List[C] = initial_population
+        self._threshold: float = threshold
+        self._max_generations: int = max_generations
+        self._mutation_chance: float = mutation_chance
+        self._crossover_chance: float = crossover_chance
+        self._selection_type: GeneticAlgorithm.SelectionType = selection_type
+        self._fitness_key: Callable = type(self._population[0]).fitness
+
+    @property
+    def population_length(self) -> int:
+        return len(self._population)
+
+    def _pick_roulette(self, wheel: List[float]) -> Tuple[C, C]:
+        return tuple(choices(self._population, weights=wheel, k=2))
+
+    # choose number of participants and take 2 that are the best ones
+    def _pick_tournament(self, num_participants: int)-> Tuple[C, C]:
+        participants: List[C] = choices(self._population, k=num_participants)
+        return tuple(nlargest(2, participants, key=self._fitness_key))
+
+    def reproduce_and_replace(self) -> None:
+        new_population: List[C] = []
+        while len(new_population) < self.population_length:
+            # keep going until the new generation is filed
+            if self._selection_type == GeneticAlgorithm.SelectionType.ROULETTE:
+                parents: Tuple[C, C] = self._pick_roulette([x.fitness() for x in self._population])
+            else :
+                parents: Tuple[C, C] = self._pick_tournament(self.population_length // 2)
+            if random() < self._crossover_chance:
+                new_population.extend(parents[0].crossover(parents[1]))
+            else:
+                new_population.extend(parents)
+        # if we have an odd number, there will be one extra. Will remove it
+        if len(new_population) > self.population_length:
+            new_population.pop()
+
+        self._population = new_population
+
+    def _mutate(self) -> None:
+        for individual in self._population:
+            if random() < self._mutation_chance:
+                individual.mutate()
+
+    def run(self) -> C:
+        best: C = max(self._population, key=self._fitness_key)
+        for generation in range(self._max_generations):
+            # early exit if it bets treshhold
+            if best.fitness() >= self._threshold:
+                return best
+            print(f"Generation {generation} Best {best.fitness()} Avg {mean(map(self._population, key = self._fitness_key))}")
+            self.reproduce_and_replace()
+            self._mutate()
+            highest: C = max(self._population, key=self._fitness_key)
+            if highest.fitness() > best.fitness():
+                best = highest # found the new best
+            return best
